3005572d566fc5b71a24f63a4c25ae914d03e85e
KorolevskiTigr2 <106224753+KorolevskiTigr2@users.noreply.github.com>
2023-07-26 17:17:44 +0000

    merge

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const button = document.querySelector("button");
 const popup = document.querySelector(".popup-wrapper");
 const close = document.querySelector(".popup-close");
 
+//functions
 button.addEventListener("click", () => {
   popup.style.display = "block";
 });
